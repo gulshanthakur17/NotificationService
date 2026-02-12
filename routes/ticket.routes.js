23379ec3093b52b8b23c921345fa9e3204444c00
Gulshan Thakur <gulshanthakur205@gmail.com>
2026-02-12 17:56:11 +0530

    Added get notification api

--- routes/ticket.routes.js
+++ routes/ticket.routes.js
@@ -1,11 +1,21 @@
-const notificationConroller = require('../controllers/ticket.controller');
+const notificationController = require('../controllers/ticket.controller');
 const ticketMiddlewares = require('../middlewares/ticket.middleware');
 
 const routes = (app) => {
     app.post(
         '/notiservice/api/v1/notifications',
         ticketMiddlewares.verifyTicketNotificationCreateRequest,
-        notificationConroller.createTicket
+        notificationController.createTicket
+    );
+
+    app.get(
+        '/notiservice/api/v1/notifications/:id',
+        notificationController.getTickets
+    );
+
+    app.get(
+        '/notiservice/api/v1/notifications',
+        notificationController.getAllTickets
     );
 }
 
