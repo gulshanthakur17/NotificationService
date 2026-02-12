8521ae06ce4c5c87ef4708ee2e7a49af4444d847
Gulshan Thakur <gulshanthakur205@gmail.com>
2026-02-12 10:37:27 +0530

    Notification service setup

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const env = require('dotenv');
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+env.config();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(process.env.PORT, async () => {
+    console.log(`Server Started on Port ${process.env.PORT} !!`);
+
+    try {
+        await mongoose.connect(process.env.DB_URL); // connected to mongo server
+        console.log("Successfully Connected to mongoose");
+    } catch (error) {
+        console.log("Not able to connect mongoose", error);    
+    }
+});
